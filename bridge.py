fixing numerical instability

bridge.py
@@ -96,10 +96,6 @@ class BrownianBridge:
 
         #plt.imshow(approximate_expectation[0, 0].detach().numpy(), cmap="gray")
         #plt.show()
-        print(approximate_expectation)
-        print(self.int_sigma_sq_t(t0, tau))
-        print(self.int_sigma_sq_t(t0, t))
-        print("\n\n")
         approximate_expectation = torch.reshape(approximate_expectation, (x_t.shape[0], self.dimension))
         drift = (approximate_expectation - x_t)/(self.int_sigma_sq_t(t0, tau) - self.int_sigma_sq_t(t0, t)) * self.sigma_t(t)**2
         return drift
@@ -198,16 +194,18 @@ class BrownianBridgeArbitrary:
         :param x_max: final value of the bridge
         :return: sample from the distribution at time t
         """
-        int_t = self.int_sigma_sq_t(0, t)
-        int_tmin = self.int_sigma_sq_t(0, t_min)
-        int_tmax = self.int_sigma_sq_t(0, t_max)
-        denom = int_tmax - int_tmin
-        first_term = int_tmax - int_t
-        second_term = int_t - int_tmin
-
+        #int_t = self.int_sigma_sq_t(0, t)
+        #int_tmin = self.int_sigma_sq_t(0, t_min)
+        #int_tmax = self.int_sigma_sq_t(0, t_max)
+        #denom = int_tmax - int_tmin
+        #first_term = int_tmax - int_t
+        #second_term = int_t - int_tmin
         ##I need to add an x_0 because these x_t is Gaussian rv with mean x_0
-        avg = 1/denom * (first_term*(x_min - x_0) + second_term*(x_max-x_0)) + x_0
-        var = int_t - 1/denom * (first_term*int_tmin + second_term*int_t)
+        #avg = 1/denom * (first_term*(x_min - x_0) + second_term*(x_max-x_0)) + x_0
+        #var = int_t - 1/denom * (first_term*int_tmin + second_term*int_t)
+
+        var = self.int_sigma_sq_t(0, t) * (1 - self.int_sigma_sq_t(0, t) / self.int_sigma_sq_t(0, t_max))
+        avg = (self.int_sigma_sq_t(0, t) / self.int_sigma_sq_t(0, t_max)) * (x_max - x_min) + x_min
         return torch.randn_like(avg) * torch.sqrt(var) + avg
 
     def compute_drift_maruyama(self, x_t, t, tau, network, dataset_n, t0=0, Unet=False):

gaussian_example.py
@@ -70,7 +70,6 @@ def run(retrain=False, mu = 1, data_path="data/gaussian_experiment_mu100/"):
         dataset = torch.tensor(np.random.normal(size = (500000,1)) + mu, dtype=torch.float32)
         times = torch.rand((500000, 1))
         perturbed_dataset = brid.sample(times, torch.randn_like(dataset), dataset)
-
         torch.save(dataset, data_path + "dataset")
         torch.save(times, data_path + "times")
         torch.save(perturbed_dataset, data_path + "perturbed_dataset")
@@ -125,8 +124,8 @@ def run(retrain=False, mu = 1, data_path="data/gaussian_experiment_mu100/"):
             loss_test = l2_loss(dataset_test, pred_test)
             print("EPOCH:", n_epoch)
             print("LOSS TEST", torch.sqrt(loss_test))
-            torch.save(net, data_path + "network")
-            wandb.log({"train_losses": all_losses, "test_loss": loss_test.detach().cpu().numpy()})
+            #torch.save(net, data_path + "network")
+            #wandb.log({"train_losses": all_losses, "test_loss": loss_test.detach().cpu().numpy()})
             net.train()
             print("\n\n\n")
 
@@ -156,7 +155,7 @@ if __name__=="__main__":
     #plt.boxplot([d1, d2], showfliers=False)
     #plt.show()
 
-    run(retrain=False, mu=100, data_path="/data/gaussian_experiment_mu100a50b5/")
+    run(retrain=True, mu=100, data_path="data/gaussian_experiment_mu100a50b5/")
 
 
 

gaussian_multiple_example.py
@@ -41,7 +41,8 @@ def generate_dataset(N_sample, dataset_number, mu=0.5):
     dataset_end = dataset_one
     #dataset_end[end_times == 2.0] = dataset_two[end_times == 2.0]
 
-    times = torch.rand((N_sample, 1)) + start_times
+    times = torch.rand((N_sample, 1))
+    #times = torch.rand((N_sample, 1)) + start_times
     #times = torch.ones((N_sample, 1))*1.99999
     """
 
@@ -90,7 +91,7 @@ def l2_loss(true_data, pred_data):
     return torch.mean((true_data - pred_data) ** 2)
 
 
-def run(network_path, retrain=False, train_size=500000, batch_size=500, epochs=250, mu1 = 50, mu2=100):
+def run(network_path, retrain=False, train_size=500000, batch_size=500, epochs=250, mu1=50, mu2=50):
     #brid = BrownianBridgeArbitrary(1, a=1, b=3)
     #brid = BrownianBridgeArbitrary(1, a=20, b=3)
     brid = BrownianBridgeArbitrary(1, a=25, b=5)
@@ -99,8 +100,7 @@ def run(network_path, retrain=False, train_size=500000, batch_size=500, epochs=2
         # Training set
         #start_times, end_times, times, dataset_start, dataset_end = generate_dataset(train_size)
         start_times1, end_times1, times1, dataset_start1, dataset_end1, dataset_num1 = generate_dataset(int(train_size/2), 1, mu1)
-        start_times2, end_times2, times2, dataset_start2, dataset_end2, dataset_num2 = generate_dataset(int(train_size/2), 2, mu2)
-
+        start_times2, end_times2, times2, dataset_start2, dataset_end2, dataset_num2 = generate_dataset(int(train_size/2), 1, mu2)
 
         perturbed_dataset1 = brid.sample_bridge(times1, start_times1, end_times1, dataset_start1, dataset_end1)
         perturbed_dataset2 = brid.sample_bridge(times2, start_times2, end_times2, dataset_start2, dataset_end2)
@@ -114,7 +114,7 @@ def run(network_path, retrain=False, train_size=500000, batch_size=500, epochs=2
 
         # Test set
         start_times_test1, end_times_test1, times_test1, dataset_start_test1, dataset_end_test1, dataset_num_test1 = generate_dataset(int(5000/2), 1, mu1)
-        start_times_test2, end_times_test2, times_test2, dataset_start_test2, dataset_end_test2, dataset_num_test2 = generate_dataset(int(5000/2), 2, mu2)
+        start_times_test2, end_times_test2, times_test2, dataset_start_test2, dataset_end_test2, dataset_num_test2 = generate_dataset(int(5000/2), 1, mu2)
         perturbed_dataset_test1 = brid.sample_bridge(times_test1, start_times_test1, end_times_test1, dataset_start_test1, dataset_end_test1)
         perturbed_dataset_test2 = brid.sample_bridge(times_test2, start_times_test2, end_times_test2, dataset_start_test2,
                                                     dataset_end_test2)
@@ -210,7 +210,7 @@ if __name__ == "__main__":
     # d2 = np.load("data/gaussian/generatedData2.npy")
     # plt.boxplot([d1, d2], showfliers=False)
     # plt.show()
-    run("data/gaussian_multiple/unet", retrain=True,  train_size=500000, batch_size=50, epochs=10000)
+    run("data/gaussian_multiple/unet", retrain=True,  train_size=500000, batch_size=500, epochs=10000)
 
 
 